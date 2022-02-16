2cfa1684458682b9c8fa93effd1b14cc16967fb0
Magma-Flavio Portela <fmspx@hotmail.com>
2022-02-16 17:15:34 -0300

    NOTIFY PROPERTY

--- src/v1/cpp/mu_app_engine_routes.h
+++ src/v1/cpp/mu_app_engine_routes.h
@@ -9,12 +9,11 @@ class Q_MU_EXPORT MUAppEngineRoutes:public MURoutes{
     Q_OBJECT
 public:
 
-    Q_PROPERTY(QString server           READ server           WRITE setServer           )
-    Q_PROPERTY(QString protocol         READ protocol         WRITE setProtocol         )
-    Q_PROPERTY(QString hostName         READ hostName         WRITE setHostName         )
-    Q_PROPERTY(int     port             READ port             WRITE setPort             )
-
-    Q_PROPERTY(QString info             READ info             WRITE setInfo     )
+    Q_PROPERTY(QString server READ server WRITE setServer NOTIFY serverChanged )
+    Q_PROPERTY(QString protocol READ protocol WRITE setProtocol NOTIFY protocoChangedl)
+    Q_PROPERTY(QString hostName READ hostName WRITE setHostName NOTIFY hostNamChangede)
+    Q_PROPERTY(int port READ port WRITE setPort NOTIFY portChanged )
+    Q_PROPERTY(QString info READ info WRITE setInfo NOTIFY infoChanged )
 
     Q_INVOKABLE explicit MUAppEngineRoutes(QObject *parent = nullptr);
     ~MUAppEngineRoutes();
@@ -28,6 +27,13 @@ public:
      */
     Q_INVOKABLE virtual QString info() const;
     Q_INVOKABLE virtual void setInfo(const QString &info);
+
+signals:
+    void serverChanged();
+    void protocolChanged();
+    void hostNameChanged();
+    void portChanged();
+    void infoChanged();
 
 private:
     void*p=nullptr;
